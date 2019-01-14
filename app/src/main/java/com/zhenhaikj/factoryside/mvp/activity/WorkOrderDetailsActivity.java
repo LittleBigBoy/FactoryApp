efdda14bcc8334c3c4b5cff0f0a173b5ad0557c7
Administrator <765316930@qq.com>
2019-01-14 11:00:57 +0800

    充值页面，待接单-查看详情

---
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/activity/WorkOrderDetailsActivity.java
@@ -0,0 +1,47 @@
+package com.zhenhaikj.factoryside.mvp.activity;
+
+import android.view.View;
+
+import com.zhenhaikj.factoryside.R;
+import com.zhenhaikj.factoryside.mvp.base.BaseActivity;
+
+
+public class WorkOrderDetailsActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_work_order_details ;
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.icon_back:
+                finish();
+                break;
+            case R.id.icon_search:
+                finish();
+                break;
+        }
+    }
+
+}
