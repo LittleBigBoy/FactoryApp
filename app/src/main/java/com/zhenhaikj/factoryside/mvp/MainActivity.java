067b29a15b3fb3a2a6677c2a588643bddae1a7d6
Administrator <765316930@qq.com>
2019-01-14 15:51:22 +0800

    首页更新

--- app/src/main/java/com/zhenhaikj/factoryside/mvp/MainActivity.java
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/MainActivity.java
@@ -1,19 +1,21 @@
 package com.zhenhaikj.factoryside.mvp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.gyf.barlibrary.ImmersionBar;
+import com.zhenhaikj.factoryside.R;
 import com.zhenhaikj.factoryside.mvp.base.BaseActivity;
 import com.zhenhaikj.factoryside.mvp.fragment.AllWorkOrdersFragment;
 import com.zhenhaikj.factoryside.mvp.fragment.DiscoveryFragment;
 import com.zhenhaikj.factoryside.mvp.fragment.HomeFragment;
 import com.zhenhaikj.factoryside.mvp.fragment.MineFragment;
+import com.zhenhaikj.factoryside.mvp.fragment.NewsFragment;
 import com.zhenhaikj.factoryside.mvp.widget.CustomViewPager;
-import com.zhenhaikj.factoryside.R;
-import com.gyf.barlibrary.ImmersionBar;
 
 import java.util.ArrayList;
 
@@ -22,6 +24,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by geyifeng on 2017/5/8.
@@ -57,6 +60,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     TextView tvMy;
     @BindView(R.id.tab_menu)
     LinearLayout tabMenu;
+    @BindView(R.id.img_message)
+    ImageView mImgMessage;
+    @BindView(R.id.tv_message)
+    TextView mTvMessage;
+    @BindView(R.id.ll_message)
+    LinearLayout ll_message;
     private ArrayList<Fragment> mFragments;
 
 
@@ -69,10 +78,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     protected void initData() {
         setSwipeBackEnable(false);
         mFragments = new ArrayList<>();
-        mFragments.add(HomeFragment.newInstance("",""));
-        mFragments.add(AllWorkOrdersFragment.newInstance("",""));
-        mFragments.add(DiscoveryFragment.newInstance("",""));
-        mFragments.add(MineFragment.newInstance("",""));
+        mFragments.add(HomeFragment.newInstance("", ""));
+        mFragments.add(NewsFragment.newInstance("",""));
+        mFragments.add(AllWorkOrdersFragment.newInstance("", ""));
+        mFragments.add(DiscoveryFragment.newInstance("", ""));
+        mFragments.add(MineFragment.newInstance("", ""));
     }
 
     @Override
@@ -85,17 +95,18 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
 
     @Override
     protected void initImmersionBar() {
-            //在BaseActivity里初始化
-            mImmersionBar = ImmersionBar.with(this);
-            mImmersionBar.statusBarDarkFont(true, 0.2f); //原理：如果当前设备支持状态栏字体变色，会设置状态栏字体为黑色，如果当前设备不支持状态栏字体变色，会使当前状态栏加上透明度，否则不执行透明度
-            mImmersionBar.statusBarColor(R.color.red);
-            mImmersionBar.fitsSystemWindows(false);
-            mImmersionBar.init();
+        //在BaseActivity里初始化
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.statusBarDarkFont(true, 0.2f); //原理：如果当前设备支持状态栏字体变色，会设置状态栏字体为黑色，如果当前设备不支持状态栏字体变色，会使当前状态栏加上透明度，否则不执行透明度
+        mImmersionBar.statusBarColor(R.color.red);
+        mImmersionBar.fitsSystemWindows(false);
+        mImmersionBar.init();
     }
 
     @Override
     protected void setListener() {
         ll_home.setOnClickListener(this);
+        ll_message.setOnClickListener(this);
         ll_category.setOnClickListener(this);
         ll_car.setOnClickListener(this);
         ll_mine.setOnClickListener(this);
@@ -110,17 +121,21 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
                 viewPager.setCurrentItem(0);
                 tabSelected(ll_home);
                 break;
-            case R.id.ll_category:
+            case R.id.ll_message:
                 viewPager.setCurrentItem(1);
+                tabSelected(ll_message);
+                break;
+            case R.id.ll_category:
+                viewPager.setCurrentItem(2);
                 tabSelected(ll_category);
                 break;
 
             case R.id.ll_car:
-                viewPager.setCurrentItem(2);
+                viewPager.setCurrentItem(3);
                 tabSelected(ll_car);
                 break;
             case R.id.ll_mine:
-                viewPager.setCurrentItem(3);
+                viewPager.setCurrentItem(4);
                 tabSelected(ll_mine);
                 break;
         }
@@ -138,12 +153,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
                 tabSelected(ll_home);
                 break;
             case 1:
-                tabSelected(ll_category);
+                tabSelected(ll_message);
                 break;
             case 2:
-                tabSelected(ll_car);
+                tabSelected(ll_category);
                 break;
             case 3:
+                tabSelected(ll_car);
+                break;
+            case 4:
                 tabSelected(ll_mine);
                 break;
         }
@@ -156,10 +174,18 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
 
     private void tabSelected(LinearLayout linearLayout) {
         ll_home.setSelected(false);
+        ll_message.setSelected(false);
         ll_category.setSelected(false);
         ll_car.setSelected(false);
         ll_mine.setSelected(false);
         linearLayout.setSelected(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 
     private class MyAdapter extends FragmentPagerAdapter {
@@ -184,6 +210,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
         super.onResume();
 
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
---
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/fragment/NewsFragment.java
@@ -0,0 +1,114 @@
+package com.zhenhaikj.factoryside.mvp.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.zhenhaikj.factoryside.R;
+import com.zhenhaikj.factoryside.mvp.base.BaseLazyFragment;
+import com.zhenhaikj.factoryside.mvp.event.UpdateEvent;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+
+public class NewsFragment extends BaseLazyFragment implements View.OnClickListener {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.magic_indicator)
+    MagicIndicator mMagicIndicator;
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+
+    private String mParam1;
+    private String mParam2;
+    private String[] mTitleDataList = new String[]{
+            "待接单", "退单处理", "已完结", "配件单", "to_be_paid",
+            "远程费单", "质保单", "未完成单", "费用变更", "留言工单"
+    };
+    private CommonNavigator commonNavigator;
+
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NewsFragment newInstance(String param1, String param2) {
+        NewsFragment fragment = new NewsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    /**
+     * 初始化沉浸式
+     */
+    @Override
+    protected void initImmersionBar() {
+        super.initImmersionBar();
+        mImmersionBar.statusBarView(mToolbar);
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.fragment_all_work_order;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.normal_dfk_ll:
+                break;
+            case R.id.normal_dfh_ll:
+                break;
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void Event(UpdateEvent event) {
+    }
+}
