06b4fed828e7d7af24be557fe45f776f6e5dad0b
Administrator <765316930@qq.com>
2019-01-19 13:31:17 +0800

    消息页面，维修页面格局修改

--- app/src/main/java/com/zhenhaikj/factoryside/mvp/fragment/NewsFragment.java
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/fragment/NewsFragment.java
@@ -1,40 +1,55 @@
 package com.zhenhaikj.factoryside.mvp.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.zhenhaikj.factoryside.R;
+import com.zhenhaikj.factoryside.mvp.adapter.MyPagerAdapter;
 import com.zhenhaikj.factoryside.mvp.base.BaseLazyFragment;
 import com.zhenhaikj.factoryside.mvp.event.UpdateEvent;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 
 public class NewsFragment extends BaseLazyFragment implements View.OnClickListener {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    @BindView(R.id.message_rv)
-    RecyclerView mMessageRv;
-    @BindView(R.id.message_et)
-    EditText mMessageEt;
-    @BindView(R.id.annex_iv)
-    ImageView mAnnexIv;
+    @BindView(R.id.magic_news)
+    MagicIndicator mMagicNews;
+//    @BindView(R.id.toolbar_news)
+//    Toolbar mToolbarNews;
+    @BindView(R.id.view_pager_news)
+    ViewPager mViewPagerNews;
 
 
     private String mParam1;
     private String mParam2;
     private String[] mTitleDataList = new String[]{
-            "待接单", "退单处理", "已完结", "配件单", "to_be_paid",
-            "远程费单", "质保单", "未完成单", "费用变更", "留言工单"
+            "消息", "通知"
     };
     private CommonNavigator commonNavigator;
+    private List<Fragment> mNewsList;
 
 
     public NewsFragment() {
@@ -74,16 +89,67 @@ public class NewsFragment extends BaseLazyFragment implements View.OnClickListen
     @Override
     protected void initImmersionBar() {
         super.initImmersionBar();
+//        mImmersionBar.statusBarView(mToolbarNews);
     }
 
     @Override
     protected int setLayoutId() {
-        return R.layout.fragment_message;
+        return R.layout.fragment_news;
     }
 
     @Override
     protected void initData() {
+        mNewsList = new ArrayList<>();
+        int i=0;
+        mNewsList.add(InformationFragment.newInstance(mTitleDataList[i],""));
+        int a=1;
+        mNewsList.add(NoticeFragment.newInstance(mTitleDataList[a],""));
+//        for (int i = 0; i < 2; i++) {
+//            mNewsList.add(MessageFragment.newInstance(mTitleDataList[i],""));
+//        }
+        mViewPagerNews.setOffscreenPageLimit(mTitleDataList.length);
+        mViewPagerNews.setAdapter(new MyPagerAdapter(this.getFragmentManager(),mTitleDataList,mNewsList));
+        commonNavigator = new CommonNavigator(mActivity);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
 
+            @Override
+            public int getCount() {
+                return mTitleDataList == null ? 0 : mTitleDataList.length;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                ColorTransitionPagerTitleView colorTransitionPagerTitleView = new ColorTransitionPagerTitleView(context);
+                colorTransitionPagerTitleView.setNormalColor(Color.WHITE);
+                colorTransitionPagerTitleView.setSelectedColor(Color.WHITE);
+                colorTransitionPagerTitleView.setTextSize(20);
+                colorTransitionPagerTitleView.setLeft(10000);
+                colorTransitionPagerTitleView.setText(mTitleDataList[index]);
+                colorTransitionPagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mViewPagerNews.setCurrentItem(index);
+                    }
+                });
+                return colorTransitionPagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+                indicator.setColors(Color.WHITE);
+                return indicator;
+            }
+        });
+        mMagicNews.setBackgroundResource(R.color.red);
+        mMagicNews.setNavigator(commonNavigator);
+        LinearLayout titleContainer = commonNavigator.getTitleContainer();
+        titleContainer.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        titleContainer.setDividerPadding(UIUtil.dip2px(mActivity, 15));
+        titleContainer.setDividerDrawable(getResources().getDrawable(R.drawable.simple_splitter));
+
+        ViewPagerHelper.bind(mMagicNews, mViewPagerNews);
     }
 
     @Override
--- app/src/main/java/com/zhenhaikj/factoryside/mvp/fragment/NoticeFragment.java
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/fragment/NoticeFragment.java
@@ -1,49 +1,24 @@
 package com.zhenhaikj.factoryside.mvp.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
-import com.scwang.smartrefresh.layout.SmartRefreshLayout;
-import com.scwang.smartrefresh.layout.api.RefreshLayout;
-import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
-import com.youth.banner.Banner;
 import com.zhenhaikj.factoryside.R;
-import com.zhenhaikj.factoryside.mvp.activity.AllWorkOrdersActivity;
-import com.zhenhaikj.factoryside.mvp.activity.BatchOrderActivity;
-import com.zhenhaikj.factoryside.mvp.activity.CustomerServiceActivity;
-import com.zhenhaikj.factoryside.mvp.activity.HomeInstallationActivity;
-import com.zhenhaikj.factoryside.mvp.activity.HomeMaintenanceActivity;
 import com.zhenhaikj.factoryside.mvp.base.BaseLazyFragment;
 import com.zhenhaikj.factoryside.mvp.base.BaseResult;
 import com.zhenhaikj.factoryside.mvp.bean.HomeData;
 import com.zhenhaikj.factoryside.mvp.contract.HomeContract;
-import com.zhenhaikj.factoryside.mvp.model.HomeModel;
-import com.zhenhaikj.factoryside.mvp.presenter.HomePresenter;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
-
-public class MessageFragment extends BaseLazyFragment implements HomeContract.View {
+public class NoticeFragment extends BaseLazyFragment implements HomeContract.View {
     private static final String ARG_PARAM1 = "param1";//
     private static final String ARG_PARAM2 = "param2";//
 
     private String mParam1;
     private String mParam2;
-    public static MessageFragment newInstance(String param1, String param2) {
-        MessageFragment fragment = new MessageFragment();
+    public static NoticeFragment newInstance(String param1, String param2) {
+        NoticeFragment fragment = new NoticeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,7 +36,7 @@ public class MessageFragment extends BaseLazyFragment implements HomeContract.Vi
 
     @Override
     protected int setLayoutId() {
-        return R.layout.fragment_message;
+        return R.layout.fragment_notice;
     }
 
     @Override
