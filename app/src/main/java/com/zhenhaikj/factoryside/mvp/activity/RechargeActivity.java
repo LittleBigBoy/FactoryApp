828d3318cd7060fb262df3296cbea2358c85ed91
youlai <761364115@qq.com>
2019-03-02 10:25:12 +0800

    支付宝配置集成， 接口异常增加提示

--- app/src/main/java/com/zhenhaikj/factoryside/mvp/activity/RechargeActivity.java
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/activity/RechargeActivity.java
@@ -1,15 +1,22 @@
 package com.zhenhaikj.factoryside.mvp.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alipay.sdk.app.PayTask;
 import com.blankj.utilcode.util.ToastUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.gyf.barlibrary.ImmersionBar;
@@ -17,10 +24,12 @@ import com.zhenhaikj.factoryside.R;
 import com.zhenhaikj.factoryside.mvp.adapter.FaceValueAdapter;
 import com.zhenhaikj.factoryside.mvp.base.BaseActivity;
 import com.zhenhaikj.factoryside.mvp.bean.FaceValue;
+import com.zhenhaikj.factoryside.mvp.bean.PayResult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -190,7 +199,66 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
                 break;
         }
     }
+    /**
+     * 支付宝支付业务
+     *
+     */
+    public void payV2() {
 
+        /**
+         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
+         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
+         * 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
+         *
+         * orderInfo的获取必须来自服务端；
+         */
+        final String orderInfo = "";
+
+        Runnable payRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(RechargeActivity.this);
+                Map<String, String> result = alipay.payV2(orderInfo, true);
+                Log.i("msp", result.toString());
+
+                Message msg = new Message();
+                msg.what = 0;
+                msg.obj = result;
+                mHandler.sendMessage(msg);
+            }
+        };
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0: {
+                    @SuppressWarnings("unchecked")
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        ToastUtils.showShort("支付成功");
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        ToastUtils.showShort("支付失败");
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        };
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/zhenhaikj/factoryside/mvp/base/BaseObserver.java
+++ app/src/main/java/com/zhenhaikj/factoryside/mvp/base/BaseObserver.java
@@ -2,6 +2,13 @@ package com.zhenhaikj.factoryside.mvp.base;
 
 import android.util.Log;
 
+import com.blankj.utilcode.util.NetworkUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.bumptech.glide.load.HttpException;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.IOException;
+
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -11,13 +18,15 @@ import io.reactivex.disposables.Disposable;
 public abstract class BaseObserver<T> implements Observer<BaseResult<T>> {
 
     private static final String TAG = "BaseObserver";
+    protected Disposable disposable;
+    private String errMsg="";
 
     protected BaseObserver() {
     }
 
     @Override
     public void onSubscribe(Disposable d) {
-
+        disposable=d;
     }
 
     @Override
@@ -28,17 +37,35 @@ public abstract class BaseObserver<T> implements Observer<BaseResult<T>> {
     @Override
     public void onError(Throwable e) {
         Log.e(TAG, "error:" + e.toString());
+        if (!NetworkUtils.isConnected()) {
+            errMsg = "网络连接出错";
+        } else if (e instanceof JsonSyntaxException) {
+            errMsg = "接口异常";
+        } else if (e instanceof HttpException) {
+            errMsg = "网络请求出错";
+        } else if (e instanceof IOException) {
+            errMsg = "网络出错";
+        }
+
+        if (disposable !=null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+        onHandleError(errMsg);
     }
 
     @Override
     public void onComplete() {
         Log.d(TAG, "onComplete");
+        if (disposable !=null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
     }
 
 
     protected abstract void onHandleSuccess(BaseResult<T> value);
 
     protected void onHandleError(String msg) {
+        ToastUtils.showShort(msg);
     }
 }
 
